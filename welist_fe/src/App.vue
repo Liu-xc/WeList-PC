7b2fd179f589bd8e123e59198300160c2675352a
Liu-xc <1392166232@qq.com>
2020-08-08 17:21:11 +0800

    index page init

--- welist_fe/src/App.vue
+++ welist_fe/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <header class="el page-header fixed top-0 bg-light">
+      <span class="welist">WeList</span>
+      <br />
+      <el-divider></el-divider>
+      <span class="slogan text-info">To The Future, Step by Step</span>
+    </header>
+    <router-view class="container-70" />
   </div>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+@import './assets/globalClass.styl'
+
+.page-header
+  padding-left 15%
+  padding-right 15%
+  padding-top 1.2rem
+  padding-bottom 1.2rem
+
+  .welist
+    font-size 2rem
+
+  .slogan
+    font-size 1rem
 </style>
---
+++ welist_fe/src/views/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <el-tabs class="tabs">
+      <el-tab-pane label="任务">
+        <todo-list></todo-list>
+      </el-tab-pane>
+      <el-tab-pane label="日志">
+        <log-list></log-list>
+      </el-tab-pane>
+      <el-tab-pane label="分享">
+        <share-list></share-list>
+      </el-tab-pane>
+      <el-tab-pane label="编辑">
+        <edit></edit>
+      </el-tab-pane>
+      <el-tab-pane label="我的">
+        <user-info-form></user-info-form>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import TodoList from '../components/TodoList'
+import LogList from '../components/LogList'
+import ShareList from '../components/ShareList'
+import UserInfoForm from '../components/UserInfoForm'
+import Edit from '../components/Edit'
+
+export default {
+  name: 'Index',
+  components: {
+    TodoList,
+    LogList,
+    ShareList,
+    UserInfoForm,
+    Edit
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container
+  margin-top 1.5rem
+</style>
